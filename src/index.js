37e7849b27a8e5d08948dc9e0dfa4850623bcbb6
JaviDAM16 <javiersanchezramos@girona.lasalle.cat>
2025-04-11 10:32:10 +0200

    hola

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router";
-import './index.css';
-import { App } from './app';
-import { About } from './about';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import "./index.css";
+import { App } from "./app";
+import { About } from "./about";
 
 let router = createBrowserRouter([
   {
@@ -23,7 +20,7 @@ let router = createBrowserRouter([
   },
 ]);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <RouterProvider router={router} />
