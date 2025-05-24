faf2f3b7c70100a003ad7cfdfa7c9e037de804f3
JaviDAM16 <javiersanchezramos@girona.lasalle.cat>
2025-05-24 15:10:02 +0200

    resposive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,42 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.scss";
 import { LanguageSelector } from "../utils/components/LanguageSelector";
 import { useLanguage } from "../utils/contexts/Language";
 import imagenes from "../images";
 
-
-
 export const Navbar = () => {
   const { t } = useLanguage();
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => setMenuOpen(!menuOpen);
+  const closeMenu = () => setMenuOpen(false);
 
   return (
     <nav className="navbar">
       <div className="navbar-logo">
-        <Link to="/inicio">
+        <Link to="/inicio" onClick={closeMenu}>
           <img src={imagenes.logozoro} alt="Zoro Shop Logo" />
         </Link>
       </div>
-      <ul className="navbar-links">
+
+      {/* Botón hamburguesa visible solo en móvil */}
+      <button className={`hamburger ${menuOpen ? "active" : ""}`} onClick={toggleMenu} aria-label="Toggle menu">
+        <span></span>
+        <span></span>
+        <span></span>
+      </button>
+
+      {/* Menú, que se oculta o muestra en móvil según estado */}
+      <ul className={`navbar-links ${menuOpen ? "open" : ""}`}>
         <li>
-          <Link to="/inicio">{t("start")}</Link>
+          <Link to="/inicio" onClick={closeMenu}>{t("start")}</Link>
         </li>
         <li>
-          <Link to="/cataleg">{t("catalog")}</Link>
+          <Link to="/cataleg" onClick={closeMenu}>{t("catalog")}</Link>
         </li>
         <li>
-          <Link to="/about">{t("about")}</Link>
+          <Link to="/about" onClick={closeMenu}>{t("about")}</Link>
         </li>
         <li>
           <LanguageSelector />
